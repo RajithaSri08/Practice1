5960805e0312bfd9bc27a3300847009c07670382
RajithaSri08 <rajitha.sri08@gmail.com>
2018-09-08 12:56:40 +0100

    test feature

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("This is for practice");
 		System.out.println("Simple Test");
-
+		System.out.println("Test feature");
 	}
 
 }
